6f38c5b3ade318a8c13159936cfce226b2e3c826
Aaryan Bhatia Ghosh <papabear.metalxray@gmail.com>
2023-04-04 22:00:58 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,261 @@
+import {
+	mobile,
+	backend,
+	creator,
+	web,
+	javascript,
+	typescript,
+	html,
+	css,
+	reactjs,
+	redux,
+	tailwind,
+	nodejs,
+	mongodb,
+	git,
+	figma,
+	docker,
+	meta,
+	starbucks,
+	tesla,
+	shopify,
+	carrent,
+	jobit,
+	tripguide,
+	threejs,
+} from "../assets";
+
+export const navLinks = [
+	{
+		id: "about",
+		title: "About",
+	},
+	{
+		id: "work",
+		title: "Work",
+	},
+	{
+		id: "contact",
+		title: "Contact",
+	},
+];
+
+const services = [
+	{
+		title: "Web Developer",
+		icon: web,
+	},
+	{
+		title: "Data Analyst",
+		icon: mobile,
+	},
+	{
+		title: "Product Manager",
+		icon: backend,
+	},
+	{
+		title: "UI/UX Designer",
+		icon: creator,
+	},
+];
+
+const technologies = [
+	{
+		name: "HTML 5",
+		icon: html,
+	},
+	{
+		name: "CSS 3",
+		icon: css,
+	},
+	{
+		name: "JavaScript",
+		icon: javascript,
+	},
+	{
+		name: "TypeScript",
+		icon: typescript,
+	},
+	{
+		name: "React JS",
+		icon: reactjs,
+	},
+	{
+		name: "Redux Toolkit",
+		icon: redux,
+	},
+	{
+		name: "Tailwind CSS",
+		icon: tailwind,
+	},
+	{
+		name: "Node JS",
+		icon: nodejs,
+	},
+	{
+		name: "MongoDB",
+		icon: mongodb,
+	},
+	{
+		name: "Three JS",
+		icon: threejs,
+	},
+	{
+		name: "git",
+		icon: git,
+	},
+	{
+		name: "figma",
+		icon: figma,
+	},
+	{
+		name: "docker",
+		icon: docker,
+	},
+];
+
+const experiences = [
+	{
+		title: "Data Science Tech Intern",
+		company_name: "GUVI",
+		icon: starbucks,
+		iconBg: "#383E56",
+		date: "March 2023 - Present",
+		points: [
+			"Handled inquiries, proposed tech solutions, and developed data skills with Python/SQL.",
+			"Collaborated on projects, taught data science concepts, and led project selection/brainstorming for future initiatives under program manager's supervision.",
+		],
+	},
+	{
+		title: "Python Backend Developer",
+		company_name: "Anyo",
+		icon: tesla,
+		iconBg: "#E6DEDD",
+		date: "March 2023 - Present",
+		points: [
+			"Built RASA chatbot backend with REST APIs, optimized performance, integrated 3rd-party services using Python, Flask,and MySQL.",
+			". Coordinated with frontend team, wrote test cases, unit tests, and utilized AWS.",
+		],
+	},
+	{
+		title: "Product Owner",
+		company_name: "Outmarch",
+		icon: shopify,
+		iconBg: "#383E56",
+		date: "Feb 2023 - April 2023",
+		points: [
+			"Defined product vision and strategy, managed the backlog, conducted user research and testing, created wireframes and prototypes, and oversaw product launch.",
+			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+			"Implementing responsive design and ensuring cross-browser compatibility.",
+			"Participating in code reviews and providing constructive feedback to other developers.",
+			" collaborated with stakeholders to meet customer needs, aligned with company objectives, managed client meetings, and wrote technical documentation.",
+		],
+	},
+	{
+		title: "Product Management Intern",
+		company_name: "Inditech",
+		icon: meta,
+		iconBg: "#E6DEDD",
+		date: "May 2022 - October 2022",
+		points: [
+			"Improved functionality and streamlined processes by developing and integrating modules while managing business and technical requirements",
+			"Led implementation of front-end and back-end features and designed wireframes to ensure successful integration of new engineering features.",
+			"Managed documentation and ensured successful implementation on relevant platforms for Inditech.",
+		],
+	},
+];
+
+const testimonials = [
+	{
+		testimonial:
+			"I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+		name: "Sara Lee",
+		designation: "CFO",
+		company: "Acme Co",
+		image: "https://randomuser.me/api/portraits/women/4.jpg",
+	},
+	{
+		testimonial:
+			"I've never met a web developer who truly cares about their clients' success like Rick does.",
+		name: "Chris Brown",
+		designation: "COO",
+		company: "DEF Corp",
+		image: "https://randomuser.me/api/portraits/men/5.jpg",
+	},
+	{
+		testimonial:
+			"After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+		name: "Lisa Wang",
+		designation: "CTO",
+		company: "456 Enterprises",
+		image: "https://randomuser.me/api/portraits/women/6.jpg",
+	},
+];
+
+const projects = [
+	{
+		name: "Car Rent",
+		description:
+			"Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+		tags: [
+			{
+				name: "react",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "mongodb",
+				color: "green-text-gradient",
+			},
+			{
+				name: "tailwind",
+				color: "pink-text-gradient",
+			},
+		],
+		image: carrent,
+		source_code_link: "https://github.com/",
+	},
+	{
+		name: "Job IT",
+		description:
+			"Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+		tags: [
+			{
+				name: "react",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "restapi",
+				color: "green-text-gradient",
+			},
+			{
+				name: "scss",
+				color: "pink-text-gradient",
+			},
+		],
+		image: jobit,
+		source_code_link: "https://github.com/",
+	},
+	{
+		name: "Trip Guide",
+		description:
+			"A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+		tags: [
+			{
+				name: "nextjs",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "supabase",
+				color: "green-text-gradient",
+			},
+			{
+				name: "css",
+				color: "pink-text-gradient",
+			},
+		],
+		image: tripguide,
+		source_code_link: "https://github.com/",
+	},
+];
+
+export { services, technologies, experiences, testimonials, projects };
